9f4db23e7b85c8920bae945e76a7b1022f8db25a
enweazudaniel <daniel.enweazu@student.aul.edu.ng>
2020-06-14 23:38:49 +0100

    Update ondemand.sh

--- ondemand.sh
+++ ondemand.sh
@@ -106,8 +106,6 @@ fstrim /system;
 # Auto-generate log directory;
 mkdir -p /storage/emulated/0/logs
 
-# Script log file location;
-LOG_FILE=/storage/emulated/0/logs
 
 export TZ=$(getprop persist.sys.timezone);
 echo $(date) > /storage/emulated/0/logs/ondemand.log
